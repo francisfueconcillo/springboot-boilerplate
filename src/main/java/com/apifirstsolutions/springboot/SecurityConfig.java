2371fc6fff6d9d6753e54acf95dbdd92b49ba3c5
Francis Fueconcillo <francisfueconcillo@gmail.com>
2024-05-21 12:30:55 +0800

    Add cascading Filters

--- src/main/java/com/apifirstsolutions/springboot/SecurityConfig.java
+++ src/main/java/com/apifirstsolutions/springboot/SecurityConfig.java
@@ -5,17 +5,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.apifirstsolutions.springboot.filters.FirstCustomFilter;
+import com.apifirstsolutions.springboot.filters.SecondCustomFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public FirstCustomFilter firstCustomFilter() {
+        return new FirstCustomFilter();
+    }
+
+    @Bean
+    public SecondCustomFilter secondCustomFilter() {
+        return new SecondCustomFilter();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // NOTE httpBasic is deprecated because its sending credential in plaintext - should use JWT, OAuth or form-based authentication
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
             .formLogin(formLogin -> formLogin.defaultSuccessUrl("/", true).permitAll())
             .httpBasic();
         
+        http.addFilterBefore(firstCustomFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAfter(secondCustomFilter(), FirstCustomFilter.class);
+
         return http.build();
     }
 }
---
+++ src/main/java/com/apifirstsolutions/springboot/filters/SecondCustomFilter.java
@@ -0,0 +1,20 @@
+package com.apifirstsolutions.springboot.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecondCustomFilter extends HttpFilter {
+
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // Add custom logic for the second filter
+        System.out.println("SecondCustomFilter is invoked");
+        chain.doFilter(request, response);
+    }
+}
